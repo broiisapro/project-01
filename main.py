baec4336677191277c4541ac5ce051e5829a536a
broiisapro <moksh.siruvani@gmail.com>
2024-11-12 10:59:00 -0500

    fixing the color code issue thingy

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 
-Light_Gray = "F5F5F5"
-Label_Color = "25265E"
+Light_Gray = "#F5F5F5"
+Label_Color = "#25265E"
+font = ('Arial', 16)
+font2 = ('Arial', 40, "bold")
 
 class Calculator:
     def __init__(self):
@@ -13,11 +15,19 @@ class Calculator:
         self.totalexpression = "0"
         self.currentexpression = "0"
 
-        self.display_frame = self.create_display_frame
-        self.buttons_frame = self.create_buttons_frame
+        self.display_frame = self.create_display_frame()
+
+        self.total_label, self.label = self.create_display_labels()
+        self.buttons_frame = self.create_buttons_frame()
     
     def create_display_labels(self):
-        total_label = tk.Label(self.display_frame, text=self.totalexpression, anchor=tk.E, bg=Light_Gray, fg=Label_Color)
+        total_label = tk.Label(self.display_frame, text=self.totalexpression, anchor=tk.E, bg=Light_Gray, fg=Label_Color, padx= 24, font=font)
+        total_label.pack(expand=True, fill="both")
+
+        label = tk.Label(self.display_frame, text=self.currentexpression, anchor=tk.E, bg=Light_Gray, fg=Label_Color, padx= 24, font=font2)
+        label.pack(expand=True, fill="both")
+
+        return total_label, label
 
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=221, bg=Light_Gray)
